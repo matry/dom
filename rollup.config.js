461643e6fcb49aabee2bba3484a5cf6a86c6cd5a
Daniel Vaughn <danielbvaughn@gmail.com>
2024-06-28 15:21:46 -0400

    adds initial commit

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'rollup';
+import terser from '@rollup/plugin-terser';
+
+export default defineConfig({
+  input: 'src/main.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'esm',
+    sourcemap: true,
+  },
+  plugins: [
+    terser(),
+  ],
+});
---
+++ src/main.js
@@ -0,0 +1,4 @@
+
+export function test(param = 'abc') {
+  console.log(`This log indicates that library consumers can access the code and can see this parameter: ${param}`);
+}
